b1ac285653d6ef9cf0e65954e46ebec5125911bd
Kitashi14 <rishavrajaryan14@gmail.com>
2022-11-05 18:40:33 +0530

    added hero component and modified loginPage

--- frontend/src/Pages/Authentication/LoginPage.js
+++ frontend/src/Pages/Authentication/LoginPage.js
@@ -2,49 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Container from "../../Components/Shared/Container";
 
-const LoginPage = ()=>{
+const LoginPage = () => {
+  console.log("login page");
 
-    console.log("login page");
-    
-    const loginGoogleButtonHandler = async ()=>{
+  const loginGoogleButtonHandler = async () => {
+    try {
+      console.log("fetching google auth link");
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_ROOT_URI}/api/auth/googleAuthlink`
+      );
 
-        try{
-            console.log("fetching google auth link")
-            const response = await fetch(`${process.env.REACT_APP_SERVER_ROOT_URI}/api/auth/googleAuthlink`);
+      const responseData = await response.json();
 
-            const responseData = await response.json();
-            
-            const googleAuthUrl = responseData.url;
+      const googleAuthUrl = responseData.url;
 
-            window.location.replace(googleAuthUrl);
-
-        }catch(err){
-            console.log(err);
-        }
+      window.location.replace(googleAuthUrl);
+    } catch (err) {
+      console.log(err);
     }
-    return(
-        <>
-        <Container>
-            <div>
-                <input type="email" required/>
-                <label>Email</label>
-            </div>
-            <br />
-            <div>
-                <input type="password" required/>
-                <label>Password</label>
-            </div>
+  };
+  return (
+    <>
+      <Container>
+        <div class=" relative ">
+          <input
+            type="email"
+            id="email"
+            class=" flex-1 w-1/2 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+            placeholder="Your email"
+            required
+          />
+          <label>Email</label>
+        </div>
+        <br />
+        <div>
+          <input type="password" required />
+          <label>Password</label>
+        </div>
 
-            <div>
-                <Link to="/verifyEmail">Create Account</Link> 
-            </div>
-            <div>
-                <button onClick={loginGoogleButtonHandler}>Login with Google</button>
-            </div>
-
-        </Container>
-        </>
-    )
-}
+        <div>
+          <Link to="/verifyEmail">Create Account</Link>
+        </div>
+        <div>
+          <button onClick={loginGoogleButtonHandler}>Login with Google</button>
+        </div>
+      </Container>
+    </>
+  );
+};
 
 export default LoginPage;
