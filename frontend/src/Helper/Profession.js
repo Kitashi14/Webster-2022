42e6e787226d5c8d3149c7ee4cd38438923ea46e
vaidyasen <ritikvaidyasen@gmail.com>
2022-11-10 20:53:12 +0530

    added profession list

---
+++ frontend/src/Helper/Profession.js
@@ -0,0 +1,44 @@
+export const profession = [
+    {
+        name:"Professor",
+        logo:"https://www.nicepng.com/png/detail/129-1290978_teacher-teacher-icon-png.png"
+    },
+    {
+        name:"Doctor",
+        logo:"https://www.nicepng.com/png/detail/131-1318685_health-icon-png-health-care-icon-png.png"
+    },
+    
+    {
+        name:"Electrician",
+        logo:"https://www.nicepng.com/png/detail/251-2517048_electrician-clipart-fridge-repair-washing-machine-parts-icon.png"
+    },
+    {
+        name:"Accountant",
+        logo:"https://www.nicepng.com/png/detail/257-2579139_create-an-account-with-acudeen-technologies-create-account.png"
+    },
+    {
+        name:"Gardener",
+        logo:"https://www.nicepng.com/png/detail/204-2049005_graphic-stock-watering-tool-for-gardening-png-icon.png"
+    },
+    {
+        name:"Librarian",
+        logo:"https://png.pngtree.com/element_our/20190528/ourlarge/pngtree-library-icon-image_1144615.jpg"
+    },
+    {
+        name:"Tailor",
+        logo:"https://cdn-icons-png.flaticon.com/512/1995/1995535.png"
+    },
+    {
+        name:"Washerman",
+        logo:"LocalLaundryServiceIcon"
+    },
+    {
+        name:"Plumber",
+        logo:"Plumbing"
+    },
+    {
+        name:"Bus Driver",
+        logo: "DirectionsBus"
+    }
+] 
+
--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -1,6 +1,7 @@
-import Container from "../Components/Shared/Container";
-import Hero from "../Components/Hero";
-import Page404 from "./Page404";
+// import Container from "../Components/Shared/Container";
+// import Hero from "../Components/Hero";
+// import Page404 from "./Page404";
+import WorkerList from "../Components/WorkerList";
 
 const LandingPage = () => {
   console.log("Landing page entered.");
@@ -9,12 +10,14 @@ const LandingPage = () => {
     <>
       {/* Navbar Already set */}
       {/* hero should take full space */}
-      <Hero />
+      {/* <Hero />
 
       <Container>
         <div>djfls</div>
         <Page404 />
-      </Container>
+      </Container> */}
+      <WorkerList/>
+
     </>
   );
 };
