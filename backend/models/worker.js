18b6b4136b24e5461f3504fd2ff3467b764588d3
Kitashi14 <rishavrajaryan14@gmail.com>
2022-11-10 20:33:22 +0530

    added worker model

---
+++ backend/models/worker.js
@@ -0,0 +1,65 @@
+//extracting mongoose module
+const mongoose = require("mongoose");
+
+//Schema for workers
+const workerSchema = new mongoose.Schema({
+
+  workerUsername: {
+    type: String,
+    required: [true, "Please enter workers username"],
+    maxlength: 25,
+  },
+  workerFirstName: {
+    type: String,
+    required: true,
+  },
+  workerLastName: {
+    type: String
+  },
+  workerEmail: {
+    type: String,
+    required: true
+  },
+  workerPhonenum: {
+    type: Number,
+    required:true
+  },
+  workerAge: {
+    type: Number,
+    required: true
+  },
+  profession: {
+    type: String,
+    required: true,
+    maxlength: 25
+  },
+  workerAddress: {
+    type: String,
+    require: [true, "Please enter address"],
+    maxlength: 500
+  },
+  location: {
+    lat: { type: Number },
+    lng: { type: Number },
+  },
+  creationTime: {
+    type: Date,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  TCR:{
+    type: Number,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  }
+});
+
+
+//exporting worker modal
+module.exports = mongoose.model("Worker", workerSchema);
