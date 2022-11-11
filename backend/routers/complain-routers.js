4556c15f60bea6c5d011af984b78f6421635ca13
Kitashi14 <rishavrajaryan14@gmail.com>
2022-11-11 05:52:19 +0530

    add more complain routes

--- backend/routers/complain-routers.js
+++ backend/routers/complain-routers.js
@@ -9,5 +9,9 @@ const complainController  = require("../controllers/complain-controllers");
 router.get("/:uid",complainController.userComplain);
 router.post("/filter",complainController.filterComplain);
 router.post("/add",complainController.addComplain);
+router.post("/update",complainController.updateComplain);
+router.get("/latest",complainController.latestComplain);
+router.get("/:cid",complainController.getComplainDetails);
+router.delete("/delete/:cid",complainController.deleteComplain);
 
 module.exports = router;
