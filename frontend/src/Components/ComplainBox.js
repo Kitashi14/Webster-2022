a8b46e88f29335c1aa3f7e784c21c9a220336b60
Kitashi14 <rishavrajaryan14@gmail.com>
2022-11-11 07:56:56 +0530

    handled console errors

--- frontend/src/Components/ComplainBox.js
+++ frontend/src/Components/ComplainBox.js
@@ -5,19 +5,19 @@ const ComplainBox = (props) => {
     <>
 
             <section>
-                <section class="text-gray-600 body-font">
-                    <div class="mx-8 mt-4 mb-8 ">
-                        <div class="p-5 bg-white flex items-center mx-auto border-b border-gray-200 rounded-lg sm:flex-row flex-col">
-                        <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex border-2 border-gray-200 rounded-full items-center justify-center  flex-shrink-0">
+                <section className="text-gray-600 body-font">
+                    <div className="mx-8 mt-4 mb-8 ">
+                        <div className="p-5 bg-white flex items-center mx-auto border-b border-gray-200 rounded-lg sm:flex-row flex-col">
+                        <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex border-2 border-gray-200 rounded-full items-center justify-center  flex-shrink-0">
                             <img
-                              src={props.item.img}class="h-3/4 w-3/4 " alt=''/>
+                              src={props.item.img}className="h-3/4 w-3/4 " alt=''/>
        
                         </div>
-                        <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                            <h1 class="text-black text-2xl title-font font-bold mb-0">{props.item.title}</h1>
-                            <p class="leading-relaxed mt-0 p-0 text-base">Profession: {props.item.profession}</p>  
-                          <p class="leading-relaxed mt-5 p-0 text-base">Date: {props.item.date}</p>
-                          <p class="leading-relaxed mt-0 p-0 text-base">By - {props.item.firstName} {props.item.lastName}</p>
+                        <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                            <h1 className="text-black text-2xl title-font font-bold mb-0">{props.item.title}</h1>
+                            <p className="leading-relaxed mt-0 p-0 text-base">Profession: {props.item.profession}</p>  
+                          <p className="leading-relaxed mt-5 p-0 text-base">Date: {props.item.date}</p>
+                          <p className="leading-relaxed mt-0 p-0 text-base">By - {props.item.firstName} {props.item.lastName}</p>
                         </div>
                         </div>
                     </div>
