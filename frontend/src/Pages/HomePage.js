b05e966a22421f7eeca84f798dabb38d1f1c28e3
Kitashi14 <rishavrajaryan14@gmail.com>
2022-11-11 07:54:21 +0530

    added fetch latest complain api

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,20 +1,55 @@
 // import Container from "../Components/Shared/Container";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 // import ComplainBox from "../Components/ComplainBox";
 import ComplainBoxes from "../Components/ComplainBoxes";
 import Hero from "../Components/Hero";
 
-
-
 const HomePage = () => {
+  const [complainsData, setComplainsData] = useState([]);
+
+  const professionInputRef = useRef();
+  const statusInputRef = useRef();
+  const userNameInputRef = useRef();
+
+  useEffect(() => {
+    const getComplainData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_ROOT_URI}/api/complain/latest`
+        );
+
+        const responseData = await response.json();
+
+        console.log("response status", response.status);
+
+        if (response.status === 200) {
+          console.log("got latest complains");
+          setComplainsData(responseData.data);
+          return;
+        } else if (response.status === 400) {
+          console.log(responseData.error);
+          alert(responseData.error);
+          return;
+        } else {
+          console.log(response.error);
+          alert("Couldn't able to fetch latest complains");
+        }
+      } catch (err) {
+        console.log(err);
+        alert("Couldn't able to fetch latest complains");
+      }
+
+      getComplainData();
+    };
+  }, []);
+
   console.log("Home page entered.");
 
   return (
     <>
       <div className="bg-gray-200">
-        <Hero/>
-        <ComplainBoxes/>
-
+        <Hero />
+        <ComplainBoxes complains={complainsData} />
       </div>
       {/* <Container>
         <div>Complains</div>
