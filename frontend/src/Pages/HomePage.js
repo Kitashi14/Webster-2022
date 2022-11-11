8b1d4c99f656248812c6292b871c8e6f21f0279f
Kitashi14 <rishavrajaryan14@gmail.com>
2022-11-11 11:09:11 +0530

    added latest complain and filter api

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 // import ComplainBox from "../Components/ComplainBox";
 import ComplainBoxes from "../Components/ComplainBoxes";
 import Hero from "../Components/Hero";
+import { profession } from "../Helper/Profession";
 
 const HomePage = () => {
   const [complainsData, setComplainsData] = useState([]);
@@ -13,6 +14,7 @@ const HomePage = () => {
 
   useEffect(() => {
     const getComplainData = async () => {
+      console.log("sending request to fetch latest complains");
       try {
         const response = await fetch(
           `${process.env.REACT_APP_SERVER_ROOT_URI}/api/complain/latest`
@@ -38,25 +40,133 @@ const HomePage = () => {
         console.log(err);
         alert("Couldn't able to fetch latest complains");
       }
-
-      getComplainData();
     };
+    getComplainData();
   }, []);
 
   console.log("Home page entered.");
+
+  const filterButtonHandler = async () => {
+    var profession = professionInputRef.current.value;
+    var status = statusInputRef.current.value;
+
+    if (status === "Any") status = null;
+    if (profession === "Any") profession = null;
+    
+    try {
+      console.log("sending request to filter complain");
+
+      const Data = {
+        profession,
+        status,
+        distance :null
+      };
+
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_ROOT_URI}/api/complain/filter`,
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(Data),
+          credentials: "include",
+        }
+      );
+
+      console.log(response.status);
+
+      const responseData = await response.json();
+
+      if (response.status === 200) {
+        console.log(responseData.message);
+        setComplainsData(responseData.data);
+        return;
+      } else if (response.status === 400) {
+        console.log(responseData.error);
+        alert(responseData.error);
+      } else {
+        throw Error("Couldn't filter complain");
+      }
+
+    } catch (err) {
+      console.log(err);
+      alert("Couldn't able to filter complains");
+    }
+  };
+
+  const usernameButtonHandler = async () => {
+    const username = userNameInputRef.current.value;
+    console.log("sdjfs");
+
+    if(username.length ===0 ){
+      alert("Enter a username");
+      return;
+    }
+
+    try {
+      console.log("sending request to filter complain of username", username);
+
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_ROOT_URI}/api/complain/username/${username}`);
+
+      console.log(response.status);
+
+      const responseData = await response.json();
+
+      if (response.status === 200) {
+        console.log(responseData.message);
+        setComplainsData(responseData.data);
+        return;
+      } else if (response.status === 400) {
+        console.log(responseData.error);
+        alert(responseData.error);
+      } else {
+        throw Error("Couldn't filter compalain");
+      }
+
+    } catch (err) {
+      console.log(err);
+      alert("Couldn't able to filter complains of user",username);
+      return;
+    }
+  };
+
 
   return (
     <>
       <div className="bg-gray-200">
         <Hero />
+        <div>
+          <select ref={professionInputRef}>
+            {profession.map((data) => {
+              return <option value={`${data.name}`} key={`${data.name}`}>{`${data.name}`}</option>;
+            })}
+            <option value="Any">Any</option>
+          </select>
+        </div>
+        <div>
+          <select ref={statusInputRef}>
+            <option value="Not Approved">Not Approved</option>
+            <option value="Approved">Approved</option>
+            <option value="Resolved">Resolved</option>
+            <option value="Any">Any</option>
+          </select>
+          <button onClick={filterButtonHandler}>Find</button>
+        </div>
+        <div className="mb-4">
+          <input
+            className="w-1/5 px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="text"
+            type="text"
+            ref={userNameInputRef}
+            placeholder="Enter username"
+          />
+          <button onClick={usernameButtonHandler}>Search</button>
+        </div>
+
         <ComplainBoxes complains={complainsData} />
-
-
       </div>
-      {/* <Container>
-        <div>Complains</div>
-        <div>gsdfhei</div>
-      </Container> */}
     </>
   );
 };
